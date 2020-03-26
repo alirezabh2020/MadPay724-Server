38c0a3bb30bcaf7a0a914058930ef12e4f60f75a
Alireza <alirezabh@gmail.com>
2020-03-26 19:55:46 +0430

    Create Models

---
+++ MadPay724.Data/DatabaseContext/MadPayDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.Data.DatabaseContext
+{
+    class MadPayDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-J74AS06\MSSQLSERVER17; Initial Catalog=MadPay724Db; Integrated Security=True;MultipleActiveResultSets=True;");
+
+        }
+    }
+}
