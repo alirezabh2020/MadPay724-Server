0bfe059624f2693eacbe5b20c220d4bcadc1fc8b
Alireza <alirezabh@gmail.com>
2020-03-26 20:22:42 +0430

    Create Database

--- MadPay724.Data/DatabaseContext/MadPayDbContext.cs
+++ MadPay724.Data/DatabaseContext/MadPayDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MadPay724.Data.Models;
 
 namespace MadPay724.Data.DatabaseContext
 {
@@ -12,5 +13,8 @@ namespace MadPay724.Data.DatabaseContext
             optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-J74AS06\MSSQLSERVER17; Initial Catalog=MadPay724Db; Integrated Security=True;MultipleActiveResultSets=True;");
 
         }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<BankCard> BankCards { get; set; }
     }
 }
